c66d58722993fa6fd56a7fca6fbb9a4a42c133e4
bxxmi <bmi6638@gmail.com>
2021-10-11 21:59:42 +0900

    feat: 구조 다시 잡기

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// const body = document.querySelector('body');
+// const color = document.querySelector('input[type=color]');
+
+// window.onload = () => {
+//   body.style.backgroundColor = color.value;
+// }
+
+// body.addEventListener('click', changeColor);
+
+// function changeColor() {
+//   body.style.backgroundColor = color.value;
+// }
