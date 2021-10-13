837c0770927505d7ce8479d61fe9e983f38fefcf
bxxmi <bmi6638@gmail.com>
2021-10-13 22:53:02 +0900

    feat: 색상 변경 기능 구현
    배경화면 색상 변경과 동시에 휘도에 따른 텍스트 색상 변경

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,55 @@
 'use strict';
 
-// const body = document.querySelector('body');
-// const color = document.querySelector('input[type=color]');
+const colorPicker = document.querySelector('input[type="color"]');
+const body = document.querySelector('body');
+const spanList = document.querySelectorAll('span');
+const moonIcon = document.querySelector('.fa-moon');
 
-// window.onload = () => {
-//   body.style.backgroundColor = color.value;
-// }
+window.addEventListener('load', defaultColor);
 
-// body.addEventListener('click', changeColor);
+// 모드 변경
 
-// function changeColor() {
-//   body.style.backgroundColor = color.value;
-// }
+// 디폴트 배경색
+function defaultColor() {
+  body.style.backgroundColor = colorPicker.value;
+  
+  colorPicker.addEventListener('input', changeColor);
+}
+
+// 배경색 제어
+function changeColor(event) {
+  const hexColor = event.target.value;
+  body.style.backgroundColor = hexColor;
+
+  hexToRGB(hexColor);
+}
+
+// 휘도 수치에 따른 텍스트 색상 제어
+function hexToRGB(hex) {
+  hex = hex.replace("#", "");
+  
+  const red = parseInt(hex.substring(0, 2), 16);
+  const green = parseInt(hex.substring(2, 4), 16);
+  const blue = parseInt(hex.substring(4, 6), 16);
+  const luma = (0.2126 * red) + (0.7152 * green) + (0.0722 * blue);
+
+  luma < 127.5 ? textWhite() : textBlack(); 
+}
+
+function textWhite() {
+  moonIcon.style.color = '#FCB738';
+  colorPicker.style.backgroundColor = 'white';
+
+  spanList.forEach(span => {
+    span.style.color = 'white';
+  });
+}
+
+function textBlack() {
+  moonIcon.style.color = '#7035DF';
+  colorPicker.style.backgroundColor = 'black';
+
+  spanList.forEach(span => {
+    span.style.color = 'black';
+  });
+}
